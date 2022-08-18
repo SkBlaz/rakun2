488e1d8ec37c73ae940480ebbb87b43e67a04ce5
bskrlj <skblaz@github.com>
2022-08-18 16:15:15 +0200

    Bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def parse_requirements(file):
 
 
 setup(name='rakun2',
-      version='0.03',
+      version='0.21',
       description=
       "RaKUn 2.0; Better faster stronger lighter",
       url='http://github.com/skblaz/mrakun',
