53687fe228a9ceb8a4da00f3fd88b042f410a674
SkBlaz <skblaz@github.com>
2022-12-07 17:42:27 +0100

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def parse_requirements(file):
 
 
 setup(name='rakun2',
-      version='0.24',
+      version='0.25',
       description=
       "RaKUn 2.0; Better faster stronger lighter",
       url='http://github.com/skblaz/rakun2',
